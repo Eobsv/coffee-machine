ee05414e6f059fb132f6481d3c1919f700788aed
Eobsv <d.eobsv@gamail.com>
2022-08-19 15:50:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# TODO Print report, water, coffee, milk, money
+# TODO Coins 1,5,10,25
+# TODO Process coins
+# TODO Check the transaction
+# TODO Make a coffee
+# TODO Refer to .pdf file downloaded. To be found in GoogleChrome PythonTabGroup
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

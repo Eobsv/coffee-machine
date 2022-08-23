05d6261241a80fec3303a1e97ceb1cac0c05acb8
Eobsv <d.eobsv@gamail.com>
2022-08-23 18:14:06 +0200

    Fulfilled TODOs..

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+import data
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -10,13 +10,67 @@
 # TODO Make a coffee
 # TODO Refer to .pdf file downloaded. To be found in GoogleChrome PythonTabGroup
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+profit = 0
+water = data.resources["water"]
+coffee = data.resources["coffee"]
+milk = data.resources["milk"]
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def print_report():
+    print(f"Money: ${profit}, water: {water}ml, coffee: {coffee}g, milk: {milk}ml")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def is_resource_sufficient(order_ingredients):
+    """Returns True if order ingredients are sufficient if not False"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= data.resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+    return True
+
+
+def process_coins():
+    """Returns total calculated from coins inserted"""
+    print("Please insert coins")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.1
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+    return total
+
+
+def is_transaction_succesful(money_received, drink_cost):
+    """True if payment is accepted, False if there is not enough money"""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the req. ingredients from the resources"""
+    for item in order_ingredients:
+        data.resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕. Enjoy!")
+
+
+is_on = True
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    if choice == "Report":
+        print_report()
+    else:
+        drink = data.MENU[choice]
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            if is_transaction_succesful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
